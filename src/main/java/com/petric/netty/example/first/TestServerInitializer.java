8a5dcef6d6416394ca447cf104340e285c25877b
usernme <jiapeng.huang@mobeid.com>
2019-02-12 18:05:00 +0800

    n

--- src/main/java/com/petric/netty/example/first/TestServerInitializer.java
+++ src/main/java/com/petric/netty/example/first/TestServerInitializer.java
@@ -12,6 +12,6 @@ public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
         ChannelPipeline pipeline = ch.pipeline();
 
         pipeline.addLast("httpServerCodec", new HttpServerCodec()); // 必须多例
-        pipeline.addLast("testHttpServegit", new TestHttpServerHandler()); // 必须多例
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler()); // 必须多例
     }
 }
