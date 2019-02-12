48c0e0ffc5f21849a83a92bf4c34b2b1de196d18
usernme <jiapeng.huang@mobeid.com>
2019-02-12 15:16:30 +0800

    init

---
+++ src/main/java/com/petric/netty/example/first/TestServer.java
@@ -0,0 +1,33 @@
+package com.petric.netty.example.first;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 充当http服务器；
+ * curl 'http://localhost:8899'
+ */
+public class TestServer {
+
+    public static void main(String[] args) throws Exception {
+
+        // NioEventLoopGroup就一个死循环；bossGroup接收连接，把连接分发给workerGroup
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
---
+++ src/main/java/com/petric/netty/example/first/TestServerInitializer.java
@@ -0,0 +1,17 @@
+package com.petric.netty.example.first;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("httpServerCodec", new HttpServerCodec()); // 必须多例
+        pipeline.addLast("testHttpServegit remote add origin git@github.com:PetricHwang/nettylearning.gitrHandler", new TestHttpServerHandler()); // 必须多例
+    }
+}
