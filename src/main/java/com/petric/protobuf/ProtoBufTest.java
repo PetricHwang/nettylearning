6c3c91c854e8aec3a7a401b7b435369ac346609f
usernme <jiapeng.huang@mobeid.com>
2019-02-22 12:08:56 +0800

    protobuf demo

---
+++ src/main/java/com/petric/protobuf/ProtoBufTest.java
@@ -0,0 +1,18 @@
+package com.petric.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class ProtoBufTest {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        DataInfo.Student student = DataInfo.Student.newBuilder()
+                .setName("李世明").setAge(20).setAddress("深圳").build();
+
+        byte[] student2ByteArray = student.toByteArray();
+        DataInfo.Student student2 = DataInfo.Student.parseFrom(student2ByteArray);
+
+        System.out.println(student2.getName());
+        System.out.println(student2.getAge());
+        System.out.println(student2.getAddress());
+
+    }
+}
