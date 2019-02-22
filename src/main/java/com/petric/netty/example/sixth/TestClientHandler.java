06b867ac99b5a83a5fa5cfdaa92cb9be500f17da
usernme <jiapeng.huang@mobeid.com>
2019-02-22 14:39:22 +0800

    netty&&protobuf

---
+++ src/main/java/com/petric/netty/example/sixth/TestClientHandler.java
@@ -0,0 +1,18 @@
+package com.petric.netty.example.sixth;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        MyDataInfo.Person person = MyDataInfo.Person.newBuilder().setName("李世民").setAge(20).setAddress("北京").build();
+        ctx.channel().writeAndFlush(person);
+    }
+}
